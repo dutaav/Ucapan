6cb966a163fb58408e0feed8029089128d89fe1f
dutaav <51811591+dutaav@users.noreply.github.com>
2023-10-13 20:33:41 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function popupku() {
-  var body = document.querySelector("body");      
-  var pWm = document.createElement("a");
-  pWm.setAttribute("href", "https://instagram.com/dutaav");
-  pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; left: 10px");
-  pWm.innerHTML = "by dutaav";
-  body.appendChild(pWm);
-}
